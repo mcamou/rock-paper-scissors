409d52f1f3d036b98ab1a8706e51e072fb3e5116
Mario Camou <mcamou@tecnoguru.com>
2015-07-22 18:47:31 +0200

    Changed to use must insead of should and mustEqual instead of ===, and added more test to the GameSpec

--- src/test/scala/com/tecnoguru/rock_paper_scissors/GameSpec.scala
+++ src/test/scala/com/tecnoguru/rock_paper_scissors/GameSpec.scala
@@ -1,11 +1,13 @@
 package com.tecnoguru.rock_paper_scissors
 
-import akka.actor.ActorSystem
+import akka.actor.{ ActorSystem, Terminated }
 import akka.testkit.{ ImplicitSender, TestKit, TestProbe }
-import com.tecnoguru.rock_paper_scissors.games.GameDefinition.Win
+import com.tecnoguru.rock_paper_scissors.games.GameDefinition.{ Tie, Win }
 import com.tecnoguru.rock_paper_scissors.games.ValidGameDefinition
 import com.tecnoguru.rock_paper_scissors.games.ValidGameDefinition.{ Item1, Item2 }
 import org.scalatest.{ BeforeAndAfterAll, WordSpecLike }
+
+import scala.concurrent.duration._
 
 class GameSpec(_system: ActorSystem) extends TestKit(_system)
     with ImplicitSender
@@ -17,17 +19,48 @@ class GameSpec(_system: ActorSystem) extends TestKit(_system)
     TestKit.shutdownActorSystem(system)
   }
 
-  "A Game" should {
-    "Accept two Items and reply with a Result to both senders" in {
-      val game = system.actorOf(Game.props(ValidGameDefinition))
-      val player1 = TestProbe()
-      val player2 = TestProbe()
-
-      game.tell(Item1, player1.ref)
-      game.tell(Item2, player2.ref)
-
-      player1.expectMsg(Win(Item1, "1 is better", Item2))
-      player2.expectMsg(Win(Item1, "1 is better", Item2))
+  "A Game" when {
+    "receiving the first message" must {
+      "not reply" in {
+        val game = system.actorOf(Game.props(ValidGameDefinition))
+        game ! Item1
+        expectNoMsg(500.millis)
+      }
     }
+
+    "receiving the second message" must {
+      "reply with a Win if one of the senders won" in new GameContext {
+        game.tell(Item1, player1.ref)
+        game.tell(Item2, player2.ref)
+
+        player1.expectMsg(Win(Item1, "1 is better", Item2))
+        player2.expectMsg(Win(Item1, "1 is better", Item2))
+      }
+
+      "reply with a Tie if the senders tied" in new GameContext {
+        game.tell(Item1, player1.ref)
+        game.tell(Item1, player2.ref)
+
+        player1.expectMsg(Tie)
+        player2.expectMsg(Tie)
+      }
+
+      "die afterwards" in new GameContext {
+        watch(game)
+
+        game.tell(Item1, player1.ref)
+        game.tell(Item2, player2.ref)
+
+        expectMsgPF(remaining) {
+          case Terminated(`game`) ⇒
+        }
+      }
+    }
+  }
+
+  trait GameContext {
+    val game = system.actorOf(Game.props(ValidGameDefinition))
+    val player1 = TestProbe()
+    val player2 = TestProbe()
   }
 }
--- src/test/scala/com/tecnoguru/rock_paper_scissors/games/GameDefinitionSpec.scala
+++ src/test/scala/com/tecnoguru/rock_paper_scissors/games/GameDefinitionSpec.scala
@@ -1,20 +1,21 @@
 package com.tecnoguru.rock_paper_scissors.games
 
 import org.scalatest.WordSpec
+import org.scalatest.MustMatchers
 
-class GameDefinitionSpec extends WordSpec {
+class GameDefinitionSpec extends WordSpec with MustMatchers {
 
-  "A Valid Game Definition" should {
-    import ValidGameDefinition._
+  "A Valid Game Definition" must {
     import GameDefinition._
+    import ValidGameDefinition._
 
     "indicate who wins" in {
-      ValidGameDefinition.check(Item1, Item2) === Win(Item1, "1 is better", Item2)
-      ValidGameDefinition.check(Item2, Item1) === Win(Item1, "1 is better", Item2)
+      ValidGameDefinition.check(Item1, Item2) mustEqual Win(Item1, "1 is better", Item2)
+      ValidGameDefinition.check(Item2, Item1) mustEqual Win(Item1, "1 is better", Item2)
     }
 
     "Indicate a tie" in {
-      ValidGameDefinition.check(Item1, Item1) === Tie
+      ValidGameDefinition.check(Item1, Item1) mustEqual Tie
     }
   }
 }
