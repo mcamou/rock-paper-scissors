bb464dd409bf31bca9a64c380d9a86230a151f3b
Mario Camou <mcamou@tecnoguru.com>
2015-07-23 17:21:42 +0200

    More work on the HTTP interface

--- src/main/scala/com/tecnoguru/rock_paper_scissors/api/ApiService.scala
+++ src/main/scala/com/tecnoguru/rock_paper_scissors/api/ApiService.scala
@@ -1,6 +1,7 @@
 package com.tecnoguru.rock_paper_scissors.api
 
 import akka.actor.ActorRefFactory
+import com.tecnoguru.rock_paper_scissors.games.GameRegistry
 import spray.http.StatusCodes.NotFound
 import spray.routing.HttpService
 
@@ -9,14 +10,23 @@ import spray.routing.HttpService
  */
 trait ApiServiceTrait extends HttpService {
   implicit val actorRefFactory: ActorRefFactory
+  val gameRegistry: GameRegistry
 
   val route = {
     get {
       complete {
         NotFound -> "Please specify a game"
       }
-    }
+    } ~
+      path(Segment) { gameName ⇒
+        gameRegistry.getGame(gameName) match {
+          case Some(game) ⇒ ???
+          case None ⇒ complete {
+            NotFound -> s"Valid game types are: ${gameRegistry.getAvailableGames.mkString(", ")}"
+          }
+        }
+      }
   }
 }
 
-class ApiService(implicit val actorRefFactory: ActorRefFactory) extends ApiServiceTrait
+class ApiService(val gameRegistry: GameRegistry)(implicit val actorRefFactory: ActorRefFactory) extends ApiServiceTrait
--- src/test/scala/com/tecnoguru/rock_paper_scissors/api/ApiServiceSpec.scala
+++ src/test/scala/com/tecnoguru/rock_paper_scissors/api/ApiServiceSpec.scala
@@ -1,16 +1,30 @@
 package com.tecnoguru.rock_paper_scissors.api
 
-import org.scalatest.{ MustMatchers, WordSpec }
+import com.tecnoguru.rock_paper_scissors.games.{ ValidGameDefinition, GameRegistry }
+import org.scalatest.{ BeforeAndAfterAll, MustMatchers, WordSpec }
 import spray.http.StatusCodes
 import spray.testkit.ScalatestRouteTest
 
-class ApiServiceSpec extends WordSpec with MustMatchers with ScalatestRouteTest with ApiServiceTrait {
+class ApiServiceSpec extends WordSpec with MustMatchers with ScalatestRouteTest with ApiServiceTrait with BeforeAndAfterAll {
   override implicit val actorRefFactory = system
+  override val gameRegistry = new GameRegistry
+
+  override def beforeAll() = {
+    gameRegistry.register("valid", ValidGameDefinition)
+  }
 
   "The API" when {
     "it receives a GET for the root document" must {
-      "reply with a 404" in {
+      "reply with a NotFound error code" in {
         Get() ~> route ~> check {
+          status mustEqual StatusCodes.NotFound
+        }
+      }
+    }
+
+    "it receives a GET for an invalid game" must {
+      "reply with a NotFound error code" in {
+        Get("/invalid") ~> route ~> check {
           status mustEqual StatusCodes.NotFound
         }
       }
